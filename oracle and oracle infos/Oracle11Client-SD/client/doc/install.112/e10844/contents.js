4c7597b48c7313a27eb2fdc757eb2778646bf57a
Jai <jai34782@gmail.com>
2021-02-26 10:35:50 +0800

    add webdev2

---
+++ oracle and oracle infos/Oracle11Client-SD/client/doc/install.112/e10844/contents.js
@@ -0,0 +1,12 @@
+var chapters = [
+  { topic: "chapter", section: "1", title: "Oracle Database Client Installation Overview", anchor: "BABEDAFD", file: "overview.htm" },
+  { topic: "chapter", section: "2", title: "Oracle Database Client Preinstallation Requirements", anchor: "CIHHCHCJ", file: "pre_install.htm" },
+  { topic: "chapter", section: "3", title: "Installing Oracle Database Client", anchor: "CIHBBHAD", file: "install.htm" },
+  { topic: "chapter", section: "4", title: "Oracle Database Client Postinstallation\u00A0Tasks", anchor: "CHDEBCCC", file: "post_install.htm" },
+  { topic: "chapter", section: "5", title: "Removing Oracle Database Client Software", anchor: "BABHIGFE", file: "deinstall.htm" },
+  { topic: "appendix", section: "A", title: "Installing Java Access Bridge", anchor: "NTCLI009", file: "jbridge.htm" },
+  { topic: "appendix", section: "B", title: "Installing Oracle Database Client Using Response Files", anchor: "CHDICDJA", file: "advance.htm" },
+  { topic: "appendix", section: "C", title: "Configuring Oracle Database Globalization Support", anchor: "CEGEAADF", file: "gblsupp.htm" },
+  { topic: "appendix", section: "D", title: "Troubleshooting the Oracle\u00A0Database\u00A0Client\u00A0Installation", anchor: "BABFDJDB", file: "ts.htm" },
+  { topic: "glossary", section: "", title: "Glossary", anchor: "CBAJADDD", file: "glossary.htm" },
+  { } ];
